aa9949365294d31a4d77691799c5612c926cbc88
landis-zou <527659969@qq.com>
2021-06-25 16:27:10 +0800

    导表工具
    基本完善可以使用，后续添加加密功能以及lua和xlua的导表方式

--- PythonSave/LandisTools/ExcelToJson.py
+++ PythonSave/LandisTools/ExcelToJson.py
@@ -1,8 +1,8 @@
-# create by landis at 2020.01.15
+# create by Landis at 2020.01.15
 # !/usr/bin/python
 # coding=utf-8
 
-import PythonFile
+import unicodedata
 import xlrd
 import json
 import xml.dom.minidom
@@ -13,6 +13,7 @@ import time
 from tkinter import messagebox
 from tkinter import *
 from tkinter import ttk
+from enum import Enum
 from tkinter.filedialog import (askopenfilename, askdirectory)
 
 # -------------------------------- 公共变量 ---------------------------------------
@@ -22,30 +23,48 @@ pub_excel_path_input = None                 # Excel输入路径
 pub_data_path_output = None                 # 转化文件输出路径
 pub_encrypt_file_path = None                # 加密文件路径
 pub_data_convert_type = None                # 转化类型选择
+pub_excel_data_type =[]
+pub_excel_select_type = '.xls .xlsx .xlsm'
+pub_excel_change_type = ["json", "xml"]
 
-# -------------------------------- ExcelToJson Tool ---------------------------------------
+class LabLv(Enum):
+    INFO = 'black'
+    WARN = 'orange'
+    ERROR = 'red'
+    SUCCESS = 'green'
+    SKIP = 'blue'
+# -------------------------------- ExcelToJson Deal Tool ---------------------------------------
 
 def dealExcelDataToXml(excel_file):
     xmlData = readExcelData(r'' + excel_file)
     ori_data = xmlData.sheets()[0]
 
-    doc = xml.dom.minidom.Document()
-    info = doc.createElement('info')
-    doc.appendChild(info)
+    data_doc = xml.dom.minidom.Document()
+    info = data_doc.createElement('info')
+    data_doc.appendChild(info)
+
+    excel_name = os.path.basename(str(excel_file))
+    file_name = excel_name.split('.')[0]
 
     for data_nrow in range(1, ori_data.nrows):
-        item = doc.createElement('item')
+        item = data_doc.createElement('item')
+        count = 0
         for data_ncol in range(0, ori_data.ncols):
             key = ori_data.cell(0, data_ncol).value
             value = ori_data.cell(data_nrow, data_ncol).value
-            if isinstance(value, float):
-                value = '%0d' % value
-            item.setAttribute(key, value)
+            if data_ncol == 0:
+                if not is_number(value):
+                    break
+            if len(str(value)) == 0:
+                showLogInfo(excel_name + '表,第' + str(data_nrow) + '行,' + key + '数值为空,请检查', LabLv.ERROR)
+                count +=1
+                continue
+            item.setAttribute(key, str(value))
+            count += 1
+        if count == ori_data.ncols:
+            info.appendChild(item)
 
-        info.appendChild(item)
-
-    saveFile('E:/Test_Save', '123', doc.toprettyxml(), "xml")
-
+    saveFile(pub_data_path_output.get(), file_name, data_doc.toprettyxml(), "xml")
 
 
 def dealExcelDataToJson(excel_file):
@@ -54,18 +73,28 @@ def dealExcelDataToJson(excel_file):
     info_name = ori_data.row_values(0)
     new_data = {}
 
-    for i in range(ori_data.nrows):
-        if i == 0:
-            continue
+    excel_name = os.path.basename(str(excel_file))
+    file_name = excel_name.split('.')[0]
+
+    for data_nrow in range(ori_data.nrows):
         count = 0
         new_cow_data = {}
-        for j in ori_data.row_values(i):
-            new_cow_data[info_name[count]] = str(j)
+        for data_ncol in ori_data.row_values(data_nrow):
+            if count == 0:
+                if not is_number(data_ncol):
+                    break
+            if len(str(data_ncol)) == 0:
+                showLogInfo(excel_name + '表,第' + str(data_nrow) + '行,' + info_name[count] + '数值为空,请检查', LabLv.ERROR)
+                count += 1
+                continue
+            new_cow_data[info_name[count]] = str(data_ncol)
             count += 1
-        new_data[ori_data.row_values(i)[0]] = new_cow_data
 
-    json_data = json.dumps(new_data, indent=4, sort_keys=True).encode("utf-8").decode('unicode_escape')
-    saveFile('E:/Test_Save', '123', json_data, "json")
+        if count == ori_data.ncols:
+            new_data[ori_data.row_values(data_nrow)[0]] = new_cow_data
+
+    json_data = json.dumps(new_data, indent=4).encode("utf-8").decode('unicode_escape')
+    saveFile(pub_data_path_output.get(), file_name, json_data, "json")
 
 
 def readExcelData(file_path):
@@ -73,63 +102,111 @@ def readExcelData(file_path):
         data = xlrd.open_workbook(file_path)
         return data
     except Exception as e:
-        print(u'excel表格读取失败：%s' % e)
+        showLogInfo(u'表格读取失败：%s' % e, LabLv.ERROR)
         return None
 
 
-def saveFile(file_path, file_name, data, filetype):
-    output = codecs.open(file_path + "/" + file_name + "." + filetype, 'w', "utf-8")
+def saveFile(file_path, file_name, data, file_type):
+    output = codecs.open(file_path + "/" + file_name + "." + file_type, 'w', "utf-8")
     output.write(data)
     output.close()
+    showLogInfo(file_name + '.' + file_type + '  已转化', LabLv.SUCCESS)
 
 
-# dealExcelDataToJson(PythonFile.testfile)
-# dealExcelDataToXml(PythonFile.testfile)
-# ---------------------------------------------------------------------------------------
+# 判断是否为数字
+def is_number(data):
+    try:
+        float(data)
+        return True
+    except ValueError:
+        pass
+    try:
+        unicodedata.numeric(data)
+        return True
+    except (TypeError, ValueError):
+        pass
+
+    return False
+
+# --------------------------------- Create Resources Folder------------------------------
+def resource_path(relative_path):
+    if getattr(sys, 'frozen', False):
+        base_path = sys._MEIPASS
+    else:
+        base_path = os.path.abspath('.')
+    return os.path.join(base_path, relative_path)
 
 
+
+# ------------------------------------- Window ------------------------------------------
 def selectPath():
     if pub_folder_select_type.get() == 1:
-        path_ = askopenfilename(filetypes=[("Excel files", ".xlsx .xls")])
+        inputFile = askopenfilename(filetypes=[('Excel files', pub_excel_select_type)])
     else:
-        path_ = askdirectory()
-    pub_excel_path_input.set(path_)
-    if len(pub_excel_path_input.get()) != 0:
-        showLogInfo('当前选择的路径为:' + pub_excel_path_input.get())
-    # if path_ != '':
-    #     excel_file_name = os.path.basename(str(path_))
-    #     excel_name = excel_file_name.split('.')[0]
-    #     excel_type = excel_file_name.split('.')[1]
+        inputFile = askdirectory()
+    if len(inputFile) != 0:
+        pub_excel_path_input.set(os.path.normpath(inputFile))
+        showLogInfo('当前选择路径文件:' + pub_excel_path_input.get())
+        normalFile = os.path.normpath(inputFile)
+        if len(pub_data_path_output.get()) == 0:
+            if os.path.isdir(inputFile):
+                pub_data_path_output.set(normalFile)
+            elif os.path.isfile(inputFile):
+                pub_data_path_output.set(os.path.dirname(normalFile))
+            showLogInfo('初始默认输出路径:' + pub_data_path_output.get())
 
 
 def outputPath():
     outputFile_ = askdirectory()
-    pub_data_path_output.set(outputFile_)
+    if len(outputFile_) != 0:
+        pub_data_path_output.set(os.path.normpath(outputFile_))
+        showLogInfo('当前输出路径：' + pub_data_path_output.get())
 
 
 def encryptFilePath():
-    messagebox.showinfo('提示','暂未支持数据加密，敬请期待')
-    # pub_encrypt_file_path.set(askopenfilename()) # 暂未使用
+    messagebox.showinfo('提示', '暂未支持数据加密，敬请期待')
+    # pub_encrypt_file_path.set(os.path.normpath(askopenfilename())) # 暂未使用
 
 
-def refreshencryptBtn(encrypt_btn, encrypt_btn_state):
+def refreshEncryptBtn(encrypt_btn, encrypt_btn_state):
     state = NORMAL if encrypt_btn_state.get() == 1 else DISABLED
     encrypt_btn['state'] = state
-    showLogInfo('123')
 
 
-def showLogInfo(info):
+def showLogInfo(msg, lab_lv=LabLv.INFO):
     show_toast_Lab.config(state=NORMAL)
     realtime = time.strftime("%H:%M:%S")
-    text_var = realtime + '    ' + info + '\n'
-    show_toast_Lab.insert('end', text_var)
+    text_var = realtime + '    ' + msg + '\n'
+    show_toast_Lab.insert('end', text_var, str(lab_lv))
     show_toast_Lab.see(END)
     show_toast_Lab.config(state=DISABLED)
 
 
 def dataBoxItemClick():
-    showLogInfo(pub_data_convert_type.get())
+    if os.path.isdir(pub_excel_path_input.get()):
+        for root, ds, fs in os.walk(pub_excel_path_input.get()):
+            for fileName in fs:
+                convertDateByExcelFile(os.path.join(root, fileName))
+    elif os.path.isfile(pub_excel_path_input.get()):
+        convertDateByExcelFile(pub_excel_path_input.get())
+    else:
+        showLogInfo('转换的文件路径未选择，请确认', LabLv.WARN)
 
+
+def convertDateByExcelFile(excel_file):
+    excel_type = os.path.splitext(excel_file)[-1][1:]
+    excel_name = os.path.basename(str(excel_file))
+    if '~$' in excel_name:
+        showLogInfo(excel_name + '缓存文件，忽略', LabLv.SKIP)
+        return
+    if len(excel_type) == 0:
+        return
+
+    if excel_type in pub_excel_select_type:
+        if pub_data_convert_type.get() == pub_excel_change_type[0]:
+            dealExcelDataToJson(excel_file)
+        elif pub_data_convert_type.get() == pub_excel_change_type[1]:
+            dealExcelDataToXml(excel_file)
 
 
 def onGUI():
@@ -140,7 +217,7 @@ def onGUI():
     Label(window, text="目标路径:").grid(row=0, column=0)
     Entry(window, textvariable=pub_excel_path_input, state='disabled').grid(row=0, column=1)
     Button(window, text="浏览", command=lambda: selectPath()).grid(row=0, column=2)
-    Checkbutton(window, text='仅文件', variable=pub_folder_select_type, onvalue=1, offvalue=0).grid(row=0, column=3)
+    Checkbutton(window, text='仅读文件', variable=pub_folder_select_type, onvalue=1, offvalue=0).grid(row=0, column=3)
 
     global pub_data_path_output
     pub_data_path_output = StringVar()
@@ -155,29 +232,31 @@ def onGUI():
     Entry(window, textvariable=pub_encrypt_file_path, state='disabled').grid(row=2, column=1)
     encrypt_btn = Button(window, text="浏览", state='disabled', command=lambda: encryptFilePath())
     encrypt_btn.grid(row=2, column=2)
-    Checkbutton(window, text='是否加密', variable=encrypt_btn_state, command=lambda: refreshencryptBtn(encrypt_btn, encrypt_btn_state), onvalue=1, offvalue=0).grid(row=2, column=3)
-
+    Checkbutton(window, text='是否加密', variable=encrypt_btn_state, command=lambda: refreshEncryptBtn(encrypt_btn, encrypt_btn_state), onvalue=1, offvalue=0).grid(row=2, column=3)
 
     global pub_data_convert_type
     pub_data_convert_type = StringVar()
-    databoxList = ttk.Combobox(window, width=8, state='readonly', textvariable=pub_data_convert_type)
-    databoxList["values"] = ("json", "xml")
-    databoxList.current(0)
-    databoxList.grid(row=3, column=3)
+    dataBoxList = ttk.Combobox(window, width=8, state='readonly', textvariable=pub_data_convert_type)
+    dataBoxList["values"] = pub_excel_change_type
+    dataBoxList.current(0)
+    dataBoxList.grid(row=3, column=3)
 
     Button(window, width=35, text="开始转换", command=lambda: dataBoxItemClick()).grid(row=3, column=0, columnspan=3)
 
     global show_toast_Lab
     show_toast_Lab = Text(window, width=50, state=DISABLED)
     show_toast_Lab.grid(row=4, column=0, columnspan=4)
-
+    # ------提前注册文本颜色
+    for lv in LabLv:
+        show_toast_Lab.tag_config(str(lv), foreground=lv.value)
 
 def onCreateWindow():
     window.title("Table Tool")
     # -------- 状态栏显示的应用图标
+    iconFile = resource_path(os.path.join('Resources', 'GetRight.ico'))
     my_ppid = 'company.product.version'
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_ppid)
-    window.iconbitmap(default=os.getcwd() + '/Resources/Icon/GetRight.ico')
+    window.iconbitmap(iconFile)
     # -------- 状态栏显示的应用图标
     window.geometry()
     onGUI()
@@ -186,5 +265,3 @@ def onCreateWindow():
 window = Tk()
 
 onCreateWindow()
-
-
--- PythonSave/LandisTools/PythonFile.py
+++ PythonSave/LandisTools/PythonFile.py
@@ -1,4 +1,3 @@
 # 这里填写自定义路径
-import os
 
 testfile = 'E:/Test_Save/123.xls'
